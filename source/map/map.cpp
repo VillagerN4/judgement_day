1665a2c3a7add56b3808861080887c73bd5c2f86
VillagerN4 <villnumber4@gmail.com>
2025-04-24 19:04:05 +0200

    Basic Map class header and implementation.

---
+++ source/map/map.cpp
@@ -0,0 +1,9 @@
+#include "map.hpp"
+
+using namespace std;
+
+Map::Map(int grid_size_x, int grid_size_y, int grid_layers) : GRID_SIZE_X(grid_size_x), GRID_SIZE_Y(grid_size_y), GRID_LAYERS(grid_layers)
+{}
+
+Map::~Map()
+{}
---
+++ source/map/map.hpp
@@ -0,0 +1,17 @@
+#ifndef MAPGRID_H
+#define MAPGRID_H
+
+class Map
+{
+private:
+    const int GRID_SIZE_X;
+    const int GRID_SIZE_Y;
+    const int GRID_LAYERS;
+public:
+    Map(int grid_size_x, int grid_size_y, int grid_layers);
+
+    ~Map();
+};
+
+
+#endif
