18408096fa5ef545338eaadcdd4808068ee13587
gruby00chomik <ajakoza@o2.pl>
2025-05-13 22:49:47 +0200

    Add files via upload

--- source/hero.cpp
+++ source/hero.cpp
@@ -1,43 +1,29 @@
-#include "Hero.hpp"
-
-Hero::Hero(float x, float y, int HP) : hp(HP), sprite(texture) {
-    if (!texture.loadFromFile("plik")) {
-        shape.setSize({50.f, 80.f});
-        shape.setFillColor(Color::Blue);
-        shape.setPosition(Vector2f(x, y));
-    } else {
-        sprite.setPosition(Vector2f(x, y));
-    }
+#include "hero.hpp"
+#include <SFML/Graphics.hpp>
+#include <cmath>
+using namespace sf;
+void Hero::handleMovement(float deltaTime, float speed, Vector2u windowSize) {
+    if (Keyboard::isKeyPressed(Keyboard::Key::A)) shape.move(Vector2f(-speed * deltaTime, 0.f));
+    if (Keyboard::isKeyPressed(Keyboard::Key::D)) shape.move(Vector2f(speed * deltaTime, 0.f));
+    
+    if (shape.getPosition().x < 0.f) shape.setPosition(Vector2f( 0.f, shape.getPosition().y));
+    if (shape.getPosition().x + shape.getSize().x > windowSize.x)
+        shape.setPosition(Vector2f(windowSize.x - shape.getSize().x, shape.getPosition().y));
 }
 
-void Hero::move(float speed, float deltaTime) {
-    if (Keyboard::isKeyPressed(Keyboard::Key::A)) {
-        if (texture.loadFromFile("plik")) {
-            sprite.move(Vector2f(-speed * deltaTime, 0));
-        } else {
-            shape.move(Vector2f(-speed * deltaTime, 0));
+void Hero::handleJumping(float deltaTime, float groundHeight) {
+    if (isJumping) {
+        shape.move(Vector2f(0.f, jumpSpeed * deltaTime));
+        jumpSpeed += 980.f * deltaTime;
+        if (shape.getPosition().y >= groundHeight) {
+            shape.setPosition(Vector2f(shape.getPosition().x, groundHeight));
+            isJumping = false;
+            jumpSpeed = 0.f;
         }
-    }
-    if (Keyboard::isKeyPressed(Keyboard::Key::D)) {
-        if (texture.loadFromFile("plik")) {
-            sprite.move(Vector2f(speed * deltaTime, 0));
-        } else {
-            shape.move(Vector2f(speed * deltaTime, 0));
-        }
-    }
-}
-
-void Hero::attack() {}
-
-void Hero::takeDamage(int damage) {
-    hp -= damage;
-    if (hp < 0) hp = 0;
-}
-
-void Hero::draw(RenderWindow &window) {
-    if (texture.loadFromFile("plik")) {
-        window.draw(sprite);
     } else {
-        window.draw(shape);
+        if (Keyboard::isKeyPressed(Keyboard::Key::Space)) {
+            jumpSpeed = -sqrt(2.f * 980.f * 300.f);
+            isJumping = true;
+        }
     }
 }
